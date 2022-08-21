c1c004bcc09f4af4d630ede8abac24e1523e1b04
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-08-21 20:11:33 -0300

    search form component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Fred" animal="cat" breed="Mix" />
-      <Pet name="Katie" animal="dog" breed="Belgium" />
-      <Pet name="Don" animal="fish" breed="Lambari" />
+      <SearchParams />
     </div>
   );
 };
