8f50c19f902c36cd9c72ebc706277293d020474b
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-08-26 11:47:02 -0300

    fetching data

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,21 @@
-import { useParams } from "react-router-dom";
 import { Component } from "react";
+import { useParams } from "react-router-dom";
 
 class Details extends Component {
   constructor(props) {
     super(props);
 
     this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
   }
 
   render() {
@@ -28,4 +38,9 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />
+}
+
+export default WrappedDetails;
