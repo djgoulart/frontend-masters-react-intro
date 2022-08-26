c9719880e84e1509f897b1f3868045113a8aaaed
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-08-26 10:33:55 -0300

    class component

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,31 @@
 import { useParams } from "react-router-dom";
+import { Component } from "react";
 
-const Details = () => {
-  const { id } = useParams();
+class Details extends Component {
+  constructor(props) {
+    super(props);
 
-  return <h1>{id}</h1>
+    this.state = { loading: true };
+  }
+
+  render() {
+    if (this.setState.loading) {
+      return <h2>loading...</h2>
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{animal} - {breed} - {city}, {state}</h2>
+        </div>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    )
+  }
 }
 
 export default Details;
